c4f9152e788e8220fcfdbebc438bdaa9b173bafe
Corentin Duchatelet <corentin.duchatelet@capgemini.com>
2019-10-07 12:08:56 +0200

    Added models

---
+++ src/main/java/models/Address.java
@@ -0,0 +1,41 @@
+package models;
+
+public class Address {
+
+    private String postalCode;
+    private String street;
+    private Integer streetNumber;
+    private String complement;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Integer getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(Integer streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+}
