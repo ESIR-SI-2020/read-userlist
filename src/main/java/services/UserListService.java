3ce7aaf1c4404ca00b07e963a9ccc1ee6ccaebff
Corentin Duchatelet <corentin.duchatelet@capgemini.com>
2019-10-07 12:25:44 +0200

    Added services

---
+++ src/main/java/services/UserListService.java
@@ -0,0 +1,15 @@
+package services;
+
+import models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserListService {
+    public List<User> getUsersList() {
+
+        List<User> usersList = new ArrayList<User>();
+        //fetch from elasticsearch
+        return usersList;
+    }
+}
